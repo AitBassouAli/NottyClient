59a3e02cab0a1b9aa2a6f228300105574535a674
khachaniAhmed <ahmedkhachani@gmail.com>
2018-12-10 22:46:15 +0100

    authentifiaction

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { UserModule } from './user-modal/user.modal';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,29 @@ import { UserService } from '../user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  users: Array<UserModule>;
+  user = new UserModule(0, 'ahmed', 'khachani');
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService , private router : Router) { }
 
   ngOnInit() {
+    this.getAllUsers();
   }
 
-  login(username, password){
-    this.userService.login(username, password).subscribe((data)=>{
-      //console.log(data);
+  getAllUsers() {
+    this.userService.getAll()
+      .subscribe(data => {
+        this.users = data,
+          console.log(data)
+      }, error => console.log(error));
+  }
+  login() {
+    this.users.forEach(element => {
+      if (element.username == this.user.username && element.password == this.user.password) {
+        this.router.navigate(["notes"]);
+      } else {
+
+      }
     });
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,20 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  uri = 'http://localhost:8080/';
+  uriUser = 'http://localhost:8080/users';
   constructor(private http: HttpClient) { }
-  
-  public login(username, password){
-    let headers = new HttpHeaders();
-    headers.set("Content-Type","application/json")
-    return this.http.post(this.uri+ 'login',JSON.stringify({username,password}),{headers:headers,observe:"response"}).pipe(
-      map(data => console.log(JSON.stringify(data.headers))
-    ));
+
+  getAll(): Observable<any> {
+    return this.http.get(`${this.uriUser}`);
   }
 }
